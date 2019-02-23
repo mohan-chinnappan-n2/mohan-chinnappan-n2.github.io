f509a300a1fabf206a06b41c50f6a023750e6bae
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2019-02-23 10:09:04 -0500

    dx slides added

---
+++ 2019/dx/js/slides.js
@@ -0,0 +1,73 @@
+
+// slides.js
+// mchinnappan
+// jshint esversion:6
+
+let index = 1;
+let slides = document.querySelectorAll(".slide");
+
+// fill the menu items
+function fillMenuItems() {
+  let container = document.getElementById('mitems');
+  let ndx = 1;
+  for (let slide of slides) {
+    //console.log(slide);
+    let li = document.createElement('li');
+    if (ndx === 1) {
+         li.setAttribute('class', 'list-group-item toc tocactive');
+    } else {
+         li.setAttribute('class', 'list-group-item toc');
+    }
+    
+    li.setAttribute('id', slide.getAttribute('id') + '_t' );
+
+    let a = document.createElement('a');
+    a.setAttribute('href', '#' + slides[ndx-1].id);
+    a.text = slide.getAttribute('id');
+    li.appendChild(a);
+
+    container.appendChild(li);
+    ndx++;
+  }
+}
+
+// setup menu
+fillMenuItems();
+
+// slide nav functions
+function gotoSlide(loc) {
+  let gotoId = slides[loc-1].id;
+  document.location.hash = gotoId;
+  let tocs = document.getElementsByClassName('toc');
+  for (let tocitem of tocs){
+      tocitem.classList.remove('tocactive');
+  }
+
+  let li = document.getElementById(gotoId + '_t');
+  li.classList.add('tocactive');
+  
+}
+function nextSlide() {
+  if ( (index+1) <= slides.length) {
+    index++;
+  } else {
+    index = 1;
+  }
+  gotoSlide(index);
+}
+
+function prevSlide() {
+  if (index > 0) {
+    index--;
+  } else {
+    index = slides.length + 1;
+  }
+
+  gotoSlide(index);
+}
+
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
+    nextSlide();
+  }
+};
