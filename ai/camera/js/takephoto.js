14bf1f2479d897805f13318357dc3ab1d03cae4a
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2019-03-10 19:34:22 -0400

    photo booth added

---
+++ ai/camera/js/takephoto.js
@@ -0,0 +1,34 @@
+// takePhoto.js
+//  
+async function takePhoto(quality) {
+      const resultImg = document.getElementById('result');
+      resultImg.style.display = 'none'; 
+
+      const captureBtn = document.getElementById('capture');
+
+      const video = document.getElementById('vid');
+      const stream = await navigator.mediaDevices.getUserMedia({video: true});
+      video.srcObject = stream;
+      await video.play();
+
+
+      // Wait for Capture to be clicked.
+      await new Promise( (resolve) => captureBtn.onclick = resolve);
+
+      const canvas = document.createElement('canvas');
+      canvas.width = video.videoWidth;
+      canvas.height = video.videoHeight;
+      canvas.getContext('2d').drawImage(video, 0, 0);
+      stream.getVideoTracks()[0].stop();
+
+      video.remove();
+      captureBtn.remove();
+      resultImg.style.display = 'block'; 
+      
+
+      return canvas.toDataURL('image/jpeg', quality);
+    }
+
+let resultEle = document.getElementById('result');
+let dataFile =  takePhoto(1.0);
+dataFile.then( data =>   resultEle.setAttribute('src', data) );
