b674227b11aa922c738454ad8f9742d787620513
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2019-04-29 22:33:18 -0400

    added file save

--- 2019/viz/seq/js/seq-app.js
+++ 2019/viz/seq/js/seq-app.js
@@ -134,6 +134,38 @@
 
   document.getElementById('inputfile').addEventListener('change', getUploadedFile);
 
+  // save to file
+ function saveSourceToFile() {
+  //var textToWrite = document.getElementById('textArea').innerHTML;
+  
+    var fileContent = editor.getValue();
+    var textFileAsBlob = new Blob([ fileContent ], { type: 'text/plain' });
+    var fileNameToSaveAs = "myseq.seq.txt";
+
+  var downloadLink = document.createElement("a");
+  downloadLink.download = fileNameToSaveAs;
+  downloadLink.innerHTML = "Download File";
+  if (window.webkitURL != null) {
+    // Chrome allows the link to be clicked without actually adding it to the DOM.
+    downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+  } else {
+    // Firefox requires the link to be added to the DOM before it can be clicked.
+    downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+    downloadLink.onclick = destroyClickedElement;
+    downloadLink.style.display = "none";
+    document.body.appendChild(downloadLink);
+  }
+
+downloadLink.click();
+}
+var saveSrcButton = document.getElementById('save');
+saveSrcButton.addEventListener('click', saveSourceToFile);
+
+
+
+
+
+
 
   
 
