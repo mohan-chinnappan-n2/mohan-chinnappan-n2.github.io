9f27251953e14afc62f62e2d09b1844185bb4f89
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2019-08-15 22:47:42 -0400

    update ea

--- 2019/stats/js/md2html.js
+++ 2019/stats/js/md2html.js
@@ -4,12 +4,19 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const converter = new showdown.Converter();
   mds.forEach((item, ndx) => {
     const mdEle = document.getElementById(item);
-    const text = mdEle.textContent.split('\n')
-    var trimmedText = [];
-    text.forEach( (titem, tndx) => {
-      trimmedText.push(titem.trim());
-    });
-    const html = converter.makeHtml(trimmedText.join('\n'));
+    const dataTrim = mdEle.getAttribute('data-trim');
+    console.log(dataTrim);
+    var text = mdEle.textContent;
+
+    if (dataTrim !== 'false') {
+        var trimmedText = [];
+        text.split('\n').forEach( (titem, tndx) => {
+          trimmedText.push(titem.trim());
+        });
+       text = trimmedText.join('\n');
+    }
+    //console.log(text);
+    const html = converter.makeHtml(text);
     mdEle.innerHTML = html;
     mdEle.style.display = 'block';
 
