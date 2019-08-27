f18339b8c6122233c20bade30f99c7983233a1ae
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2019-08-27 07:53:28 -0400

    md file add

---
+++ 2019/ea/md/ea-filters-selections.md
@@ -0,0 +1,12 @@
+
+- 2 ways you can power these selections and filters; 
+     1. steps that are powered by your dataset (dynamic selection)
+     2. static steps where you define a static value you can use in other steps.
+
+- The dynamic selection only work  on filters
+- The static steps can work on
+    -  filters
+    -  groupings
+    -  measures
+    -  limit 
+    -  order
--- 2019/stats/js/md2html.js
+++ 2019/stats/js/md2html.js
@@ -1,7 +1,54 @@
 // md2html.js
 // mohan chinnappan
+
+/* jshint esversion: 6 */
+
+
+let readURL = async (url) => {
+  try {
+      const response = await fetch(`${url}`);
+      return await response.text();
+  } catch (err) {
+      console.log('fetch failed', err);
+  }
+}
+
+
+
+const mdElements = document.getElementsByClassName('md');
+var mds = [];
+for (var i = 0; i < mdElements.length; i++) {
+    const ele = mdElements[i];
+    mds.push(ele.getAttribute('id'));
+};
+
+const mdFileElements = document.getElementsByClassName('md-file');
+var fileMds = [];
+for (var i = 0; i < mdFileElements.length; i++) {
+    const ele = mdFileElements[i];
+    fileMds.push(ele.getAttribute('id'));
+};
+
+
+const converter = new showdown.Converter();
+
+let md2html = (id) => {
+  const mdEle = document.getElementById(id);
+  const url = mdEle.getAttribute('data-url');
+  const mdContent =  readURL(url);
+  mdContent.then ( (mdTxt) => {
+    const html = converter.makeHtml(mdTxt);
+    mdEle.innerHTML = html;
+  });
+}
+
+
+
+fileMds.forEach( (id) => {
+    md2html(id);
+})
+
 document.addEventListener('DOMContentLoaded', (event) => {
-  const converter = new showdown.Converter();
   mds.forEach((item, ndx) => {
     const mdEle = document.getElementById(item);
     const dataTrim = mdEle.getAttribute('data-trim');
@@ -21,4 +68,5 @@ document.addEventListener('DOMContentLoaded', (event) => {
     mdEle.style.display = 'block';
 
   });
+
 });
