afd07bc497984264a36953b5e35e6573b662fcf3
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2020-03-23 14:21:50 -0400

    erlang added

--- 2020/erlang/hello.erl
+++ 2020/erlang/hello.erl
@@ -1,4 +1,4 @@
 -module(hello).
 -export([hello_world/0]).
 
-hello_world() -> io:fwrite("hello, world\n").
+hello_world() -> io:fwrite("hello, world from Erlang in the memory of Joe Armstrong!\n").
