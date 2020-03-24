3c80ae2de9e13ba2f40108ae98b1bf7441bd6038
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2020-03-23 21:55:41 -0400

    drawing added

--- 2020/erlang/hello.erl
+++ 2020/erlang/hello.erl
@@ -1,4 +1,23 @@
+% load the file hello.erl
 -module(hello).
+% export function hello_world, which takes no args
 -export([hello_world/0]).
+% export function double, which takes one arg
+-export([double/1]).
+
+-export([fac/1]).
+-export([convert/2]).
+
 
 hello_world() -> io:fwrite("hello, world from Erlang in the memory of Joe Armstrong!\n").
+
+double(X) -> 2 * X.
+
+
+fac(1) -> 1; 
+fac(N) -> N * fac(N - 1).
+
+
+convert(M, inch) -> M / 2.54;
+convert(N, centimeter) -> N * 2.54.
+
