765c1242ea23392a37c29528c7f3072f70a7d061
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2020-03-31 21:07:22 -0400

    form generator added

--- 2020/forms/js/invoice.js
+++ 2020/forms/js/invoice.js
@@ -11,20 +11,12 @@ const sampleInvoiceData = {
     invNum : 343,
     
     currency: '$',
-    logoUrl: 'https://mohan-chinnappan-n2.github.io/2020/forms/img/logo-s.png',
     dueDateDaysFromNow : 30,
 
-    fromAddress : `
-    GreenWorld, Inc.<br>
-    12345 Sunny Road<br>
-    Manchester, NH 12345
-    `,
-
-    toAddress : `
-    ABC Corp.<br>
-    John Doe<br>
-    john@example.com
-    `,
+    logoUrl: 'https://mohan-chinnappan-n2.github.io/2020/forms/img/logo-s.png',
+    
+    fromAddress : `GreenWorld, Inc.<br>12345 Sunny Road<br> Manchester, NH 12345 `,
+    toAddress : `ABC Corp.<br> John Doe<br> john@example.com `,
 
     payment: { method: 'Check', details: '' },
 
