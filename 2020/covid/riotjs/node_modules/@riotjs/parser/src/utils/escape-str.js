b169909be45b529a4e8fe670a2144b6271d425c3
mohan.chinnappan.n2 <mohan.chinnappan.n@gmail.com>
2020-04-02 14:50:56 -0400

    cc added

--- 2020/covid/riotjs/node_modules/@riotjs/parser/src/utils/escape-str.js
+++
@@ -1,7 +0,0 @@
-/**
- * Escape special characters in a given string, in preparation to create a regex.
- *
- * @param   {string} str - Raw string
- * @returns {string} Escaped string.
- */
-export default (str) => str.replace(/(?=[-[\](){^*+?.$|\\])/g, '\\')
