b82bcc978d518a4584c6769f827e22aa0253f0db
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2021-08-21 17:51:24 -0400

    data util app added

--- data/util/js/app.js
+++ data/util/js/app.js
@@ -212,17 +212,6 @@ const sampleXML = `<?xml version="1.0" encoding="UTF-8"?>
 `;
 
 
-// handson table
-
-const carData = [
-  ['year', 'Tesla', 'Volvo', 'Toyota', 'Ford'],
-  ['2019', 10, 11, 12, 13],
-  ['2020', 20, 11, 14, 13],
-  ['2021', 30, 15, 12, 13]
-];
-
-
-
 
 
 const xmlArea = document.getElementById('xmlArea');
