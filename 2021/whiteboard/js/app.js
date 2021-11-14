705d5ff3e582dc391c5a75fbe95b9b15526c4a7d
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2021-11-14 15:18:11 -0500

    wb updated

--- 2021/whiteboard/js/app.js
+++ 2021/whiteboard/js/app.js
@@ -153,6 +153,30 @@ undoBtn.addEventListener('click', undo );
 
 
 
+// SHAPES
+
+drawRect = (x, y, w, h) => {
+    context.beginPath();
+    context.rect(x, y, w, h);
+    context.stroke();
+}
+
+
+// SAVE 
+
+const  renderImage = base64URL => {
+    const win = window.open();
+    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+}
+
+
+const saveBtn =  document.getElementById('save');
+saveBtn.addEventListener('click', event => {
+    const imageUrl = canvas.toDataURL("image/png");
+    renderImage(imageUrl);
+
+})
+
 
 
 
