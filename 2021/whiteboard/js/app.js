8f1b7825a8157f2699f4d324aebc8cbc4b7e8d8d
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2021-11-14 17:58:13 -0500

    wb updated

--- 2021/whiteboard/js/app.js
+++ 2021/whiteboard/js/app.js
@@ -1,182 +1,214 @@
 // app.js
 
+// mohan chinnappan
+
+// refer: https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
 // refer: https://www.youtube.com/watch?v=wCwKkT1P7vY
 
-
 // CANVAS GEOMETRY
-const widthPercent = .8;
+const widthPercent = 0.8;
 
-const canvas = document.getElementById('canvas');
+const canvas = document.getElementById("canvas");
 canvas.width = window.innerWidth * widthPercent;
-canvas.height = window.innerHeight ;
+canvas.height = window.innerHeight;
 
 // CONTEXT
 
-let initBgColor = 'white';
+let initBgColor = "white";
 let wbContent = [];
 let wbIndex = -1;
 
-let context = canvas.getContext('2d');
+let shapeSelected = 'freehand';
+let isShapeDrawn = false;
+let startX = 0;
+let startY = 0;
+
+let context = canvas.getContext("2d");
 context.fillStyle = initBgColor;
-context.fillRect (0, 0, canvas.width, canvas.height );
+context.fillRect(0, 0, canvas.width, canvas.height);
 
 // DRAWING
 
-let drawColor = 'black';
+let drawColor = "black";
 let penThick = 10;
 let isDrawing = false;
 
+const getLocation = (event) => {
+  return [event.clientX - canvas.offsetLeft, event.clientY - canvas.offsetTop];
+};
 
-const getLocation = event => {
-    return [event.clientX - canvas.offsetLeft,  event.clientY - canvas.offsetTop];
-}
+const start = (event) => {
+  isDrawing = true;
+  context.beginPath();
+  const loc = getLocation(event);
+  startX = loc[0];
+  startY = loc[1];
+  context.moveTo(loc[0], loc[1]);
 
-const start = event => {
-    isDrawing = true;
-    context.beginPath();
-    const loc = getLocation(event);
-    context.moveTo(loc[0], loc[1]);
+  event.preventDefault();
+};
 
-    event.preventDefault();
-}
+const draw = (event) => {
+  const loc = getLocation(event);
+  if (isDrawing &&  shapeSelected === 'freehand') {
 
-const draw = event => {
-    const loc = getLocation(event);
+    context.lineTo(loc[0], loc[1]);
 
-    if (isDrawing) {
-        context.lineTo(loc[0], loc[1] );
+    context.strokeStyle = drawColor;
+    // line attributes
+    context.lineWidth = penThick;
+    context.lineCap = "round";
+    context.lineJoin = "round";
 
-        context.strokeStyle = drawColor;
-        // line attributes
-        context.lineWidth = penThick;
-        context.lineCap = "round";
-        context.lineJoin = "round";
+    context.stroke();
+  }
+  event.preventDefault();
+};
 
+const stop = (event) => {
+  const loc = getLocation(event);
+  if (isDrawing) {
+    if (shapeSelected === "rect" || shapeSelected === 'circle') {
+      const width = loc[0] - startX;
+      const height = loc[1] - startY;
+
+      // draw a new rect from the start position
+      // to the current mouse position
+      context.strokeStyle = drawColor;
+      // line attributes
+      context.lineWidth = penThick;
+      context.lineCap = "round";
+      context.lineJoin = "round";
+      if (shapeSelected === 'rect')  {
+          context.strokeRect(startX, startY, width, height);
+      }
+      else if (shapeSelected === 'circle') {
+        // alert('circle');
+        context.beginPath();
+        const startAngle = 0;
+        const endAngle = 2 * Math.PI;
+        context.arc(startX, startY, width, startAngle, endAngle);
         context.stroke();
+
+
+      }  
+       
+    } else {
+      context.stroke();
+      context.closePath(); //end the drawing
     }
-    event.preventDefault();
+    isDrawing = false;
+  }
+  event.preventDefault();
 
-    
-}
+  // for undo feature store the image data
+  if (event.type !== "mouseout") {
+    const imgData = context.getImageData(0, 0, canvas.width, canvas.height);
+    wbContent.push(imgData);
+    wbIndex++;
+  }
+};
 
-const stop = event => {
-    if (isDrawing) {
-        context.stroke();
-        context.closePath(); //end the drawing
-        isDrawing = false;
-    }
-    event.preventDefault();
+canvas.addEventListener("touchstart", start, false);
+canvas.addEventListener("touchmove", draw, false);
+canvas.addEventListener("touchend", stop, false);
 
-    // for undo feature store the image data
-    if (event.type !== 'mouseout') {
-        const imgData = context.getImageData(0, 0, canvas.width, canvas.height); 
-        wbContent.push(imgData);
-        wbIndex++;
-    }
+canvas.addEventListener("mousedown", start, false);
+canvas.addEventListener("mousemove", draw, false);
 
-
-}
-
-canvas.addEventListener('touchstart', start, false);
-canvas.addEventListener('touchmove', draw, false);
-canvas.addEventListener('touchend', stop, false);
-
-
-canvas.addEventListener('mousedown', start, false);
-canvas.addEventListener('mousemove', draw, false);
-
-canvas.addEventListener('mouseup', stop, false);
-canvas.addEventListener('mouseout', stop, false);
-
+canvas.addEventListener("mouseup", stop, false);
+canvas.addEventListener("mouseout", stop, false);
 
 // COLORS
 
 // color change handler
-const colorElements = document.querySelectorAll('.color');
+const colorElements = document.querySelectorAll(".color");
 for (const ele of colorElements) {
-    ele.addEventListener('click', event => {
-        const target = event.target;
-        drawColor = target.dataset.color;;
-    })
+  ele.addEventListener("click", (event) => {
+    const target = event.target;
+    drawColor = target.dataset.color;
+  });
 }
 
 // color via color selector
-const colorSelEle = document.getElementById('color-sel');
-colorSelEle.addEventListener('input', event => {
-    const target = event.target;
-    drawColor = target.value;
-
-    
+const colorSelEle = document.getElementById("color-sel");
+colorSelEle.addEventListener("input", (event) => {
+  const target = event.target;
+  drawColor = target.value;
 });
 
 // LINE THICKNESS
 
-const penThickEle = document.getElementById('pen-thick');
-penThickEle.addEventListener('input', event => {
-    const target = event.target;
-    penThick = target.value;
+const penThickEle = document.getElementById("pen-thick");
+penThickEle.addEventListener("input", (event) => {
+  const target = event.target;
+  penThick = target.value;
 });
-
 
 // CLEAR
 
-const clearBtn = document.getElementById('clear');
-const clear = event => {
-    context.fillStyle = initBgColor;
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    context.fillRect(0, 0, canvas.width, canvas.height);
+const clearBtn = document.getElementById("clear");
+const clear = (event) => {
+  context.fillStyle = initBgColor;
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.fillRect(0, 0, canvas.width, canvas.height);
 
-    // reset
-    wbContent = [];
-    wbIndex = -1;
-
-
-}
-clearBtn.addEventListener('click', clear );
-
+  // reset
+  wbContent = [];
+  wbIndex = -1;
+};
+clearBtn.addEventListener("click", clear);
 
 // UNDO
 
-const undoBtn = document.getElementById('undo');
+const undoBtn = document.getElementById("undo");
 
-const undo = event => {
-    if (wbIndex <= 0) {
-        clear();
-    } else {
-        wbContent.pop();
-        wbIndex--;
-        // put n-1 position image
-        context.putImageData(wbContent[wbIndex], 0, 0);
-    }
-}
-undoBtn.addEventListener('click', undo );
+const undo = (event) => {
+  if (wbIndex <= 0) {
+    clear();
+  } else {
+    wbContent.pop();
+    wbIndex--;
+    // put n-1 position image
+    context.putImageData(wbContent[wbIndex], 0, 0);
+  }
+};
+undoBtn.addEventListener("click", undo);
 
+// SAVE
 
+const renderImage = (base64URL) => {
+  const win = window.open();
+  win.document.write(
+    '<iframe src="' +
+      base64URL +
+      '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>'
+  );
+};
+
+const saveBtn = document.getElementById("save");
+saveBtn.addEventListener("click", (event) => {
+  const imageUrl = canvas.toDataURL("image/png");
+  renderImage(imageUrl);
+});
 
 // SHAPES
 
-drawRect = (x, y, w, h) => {
-    context.beginPath();
-    context.rect(x, y, w, h);
-    context.stroke();
-}
+// rect
+const rectBtn = document.getElementById("rect");
+rectBtn.addEventListener("click", (event) => {
+  shapeSelected = "rect";
+  isShapeDrawn = false;
+});
 
+// freehand
+const freehandBtn = document.getElementById("freehand");
+freehandBtn.addEventListener("click", (event) => {
+  shapeSelected = "freehand";
+});
 
-// SAVE 
-
-const  renderImage = base64URL => {
-    const win = window.open();
-    win.document.write('<iframe src="' + base64URL  + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
-}
-
-
-const saveBtn =  document.getElementById('save');
-saveBtn.addEventListener('click', event => {
-    const imageUrl = canvas.toDataURL("image/png");
-    renderImage(imageUrl);
-
-})
-
-
-
-
+// circle
+const circleBtn = document.getElementById("circle");
+circleBtn.addEventListener("click", (event) => {
+  shapeSelected = "circle";
+});
