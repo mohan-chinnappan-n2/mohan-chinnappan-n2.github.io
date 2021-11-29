e1934a71c5a8d53bff38fd9fbdaed63c3f299581
mohan-chinnappan-n <mohan.chinnappan.n@gmail.com>
2021-11-29 15:18:32 -0500

    wc added

---
+++ 2021/v2mom/js/app.js
@@ -0,0 +1,7 @@
+// app.js
+// mohan chinnappan
+
+const spec = "https://raw.githubusercontent.com/mohan-chinnappan-n/thirukkural-songs/master/viz/clock.json";
+vegaEmbed('#clock', spec).then(function (result) {
+
+}).catch(console.error);
